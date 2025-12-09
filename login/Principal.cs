c62b774f7d65491d20e672e44f3c406e1bc2cf8c
Antonyrs07 <rosj090707hgtmnsa8@cbtis217.edu.mx>
2025-12-08 18:27:34 -0600

    Principal

---
+++ login/Principal.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: CC2_PC29
+ * Fecha: 08/12/2025
+ * Hora: 06:08 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace login
+{
+	/// <summary>
+	/// Description of Principal.
+	/// </summary>
+	public partial class Principal : Form
+	{
+		public Principal()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+	}
+}
